8225511eb6351fd603e9b8fbc725689acd71746d
jaknixdq <88882646+jaknixdq@users.noreply.github.com>
2021-09-06 17:58:50 +0800

    Update build_kernel.sh

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 #内核源码地址
-KERNEL_URL=https://github.com/color597/android_kernel_oppo_r11
+KERNEL_URL=https://github.com/tao1996/oppo_r11s_kernel-10
 #内核defconfig
 KERNEL_DEFCONFIG=sdm660_perf_defconfig
 
 #谷歌GCC4.9
 echo "|| Cloning GCC ||"
 
-git clone -b master https://github.com/color597/android_kernel_oppo_r11 gcc
+git clone -b master https://github.com/tao1996/oppo_r11s_kernel-10 gcc
 
 cd ./gcc
 tar -cvpzf /home/runner/gcc32.tar.gz ./
